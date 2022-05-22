55ee145d465f999911cbdd6bc464888a1509eedc
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-05-22 18:47:20 -0300

    criação do getstatic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
 import Link from "next/link";
 import Head from "next/head";
-import commonStyles from "../styles/common.module.scss";
 import { FiCalendar, FiUser } from "react-icons/fi";
 import styles from "./home.module.scss";
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../Services/prismic";
+interface Post {
+  uid?: string;
+  first_publication_date: string | null;
+  data: {
+    title: string;
+    subtitle: string;
+    author: string;
+  };
+}
+
+interface PostPagination {
+  next_page: string;
+  results: Post[];
+}
+
+interface HomeProps {
+  postsPagination: PostPagination;
+  preview: boolean;
+}
 export default function Home() {
   return (
     <>
@@ -52,3 +72,22 @@ export default function Home() {
     </>
   );
 }
+export const getStaticProps: GetStaticProps = async ({
+  preview = false,
+  previewData,
+}) => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.getByUID('document.type', 'post', {
+    fetch: ["post.title"],
+    pageSize: 1,
+  })
+console.log(postsResponse)
+ 
+  return {
+    props: {
+      postsResponse
+    },
+    revalidate: 1800, // 30 minutos
+  };
+};
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../../Services/prismic";
 import styles from "./styles.module.scss";
+
 
 export default function Post() {
   return (
@@ -6,37 +9,11 @@ export default function Post() {
       <main className={styles.main}>
         <div className={styles.container}>
           <h1>Como utilizar hooks</h1>
-          <h3>
+          <p>
             Um breve arquivo sobre a origem da ferramenta que facilitou o acesso
             de databases e ampliou produtividade para o ecossistema
             JavaScript/TypeScript
-          </h3>
-          <div className={styles.author}>
-            <span>19 abr 2021</span>
-
-            <span>Rosana Moreira</span>
-          </div>
-        </div>
-        <div className={styles.container}>
-          <h1>Como utilizar hooks</h1>
-          <h3>
-            Um breve arquivo sobre a origem da ferramenta que facilitou o acesso
-            de databases e ampliou produtividade para o ecossistema
-            JavaScript/TypeScript
-          </h3>
-          <div className={styles.author}>
-            <span>
-             19 abr 2021
-            </span>
-
-            <span>
-            
-              Rosana Moreira
-            </span>
-          </div>
-        </div>
-        <div className={styles.morePosts}>
-          <button type="button">carregar mais posts</button>
+          </p>
         </div>
       </main>
     </>
